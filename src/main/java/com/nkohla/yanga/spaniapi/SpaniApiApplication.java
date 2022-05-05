079beee1e6fa187bf5c77cff93f1f1e42938467c
yangaxnkohla <yanga.nkohla@yahoo.com>
2022-05-05 19:19:50 +0200

    Added Swagger UI

--- src/main/java/com/nkohla/yanga/spaniapi/SpaniApiApplication.java
+++ src/main/java/com/nkohla/yanga/spaniapi/SpaniApiApplication.java
@@ -2,12 +2,30 @@ package com.nkohla.yanga.spaniapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpaniApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaniApiApplication.class, args);
+	}
+
+	// http://localhost:port/swagger-ui.html
+	@Bean
+	public Docket SpaniApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.nkohla.yanga.spaniapi")).build();
 	}
 
 }
